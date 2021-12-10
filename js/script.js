7c2c8cfd76fabf3628e4849764aa05caf3bb2836
mamba0328 <valentindjejylej@gmail.com>
2021-12-10 19:14:11 +0200

    style color has been changed a ltlm functionality was added to every button but operators

--- js/script.js
+++ js/script.js
@@ -4,14 +4,50 @@
 3.Add text.content to the screen div.
 */
 
-//DOM section 
+
+//Returns a value of the numberbutton on event
 
 const bottomButtons = document.querySelector('#numberButtons'); //takes a div with the number buttons
 const numberButton = bottomButtons.querySelectorAll('button') //takes each button from that div
+const screen = document.getElementById('screen')
+const button1 = document.querySelector('#dot')
+
+let valueOnScreen = '';  
 
 numberButton.forEach((button) => { 
- button.addEventListener('click', (e) => {
-     console.log(e.target.id)
-     return e.target.id
- })
-})
+ button.addEventListener('click', function() {
+     valueOnScreen += button.id;
+     screen.innerText = valueOnScreen;
+     return valueOnScreen
+ });
+});
+
+//reset button which clears the screen and variable
+const MC = document.getElementById('MC'); 
+MC.addEventListener('click', () => { 
+    valueOnScreen = ''; 
+    screen.innerText = '';
+});
+
+//change to negativ or positive num
+const neg = document.getElementById('negative'); 
+neg.addEventListener('click', () => { 
+
+    if (typeof(valueOnScreen) === 'string') {
+        if (valueOnScreen.includes('-')) { 
+         valueOnScreen = valueOnScreen.replace('-','');
+        } else { 
+          valueOnScreen = '-' + valueOnScreen;
+     }
+    }
+    screen.innerText = valueOnScreen;
+
+});
+
+//changes input value to percantages 
+const percentage = document.getElementById('percentage');
+percentage.addEventListener('click', () => { 
+    valueOnScreen = parseInt(valueOnScreen)/100; 
+    valueOnScreen = valueOnScreen.toString();
+    screen.innerText = valueOnScreen;
+});
